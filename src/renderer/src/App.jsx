209ec833ca982aba725bcddd4f35c888ecfc1ab2
JohanCacerez <johancacerez10@gmail.com>
2024-07-11 09:34:34 -0600

    react route add compatibility with mac and windows

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import MenuPage from './pages/MenuPage'
 import ControlPane from './pages/ControlPanePages/ControlPaneMenu'
@@ -6,14 +6,14 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<MenuPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/controlpane" element={<ControlPane />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
--- src/renderer/src/backend/dbService.js
+++ src/renderer/src/backend/dbService.js
@@ -1,54 +1,76 @@
-// dbService.js
-
 import { join } from 'path'
-import { Database } from 'sqlite3'
+import Database from 'better-sqlite3'
 import { app } from 'electron'
 
 const dbPath = join(app.getPath('userData'), 'database.sqlite')
-console.log(dbPath)
-const db = new Database(dbPath)
+const db = Database(dbPath)
 
 export const initializeDatabase = () => {
-  db.serialize(() => {
-    db.run('PRAGMA foreign_keys = ON')
-    db.run(
-      'CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, password TEXT, range TEXT, create_at DEFAULT CURRENT_TIMESTAMP )'
+  db.exec('PRAGMA foreign_keys = ON')
+
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      password TEXT,
+      range TEXT,
+      create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
-    db.run(
-      'CREATE TABLE IF NOT EXISTS magazines (id INTEGER PRIMARY KEY, name TEXT, size TEXT, status TEXT, create_at DEFAULT CURRENT_TIMESTAMP )'
+  `)
+
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS magazines (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      size TEXT,
+      status TEXT,
+      create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
-    db.run(
-      'CREATE TABLE IF NOT EXISTS dollies (id INTEGER PRIMARY KEY, name TEXT, status TEXT, create_at DEFAULT CURRENT_TIMESTAMP )'
+  `)
+
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS dollies (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      status TEXT,
+      create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
-    db.run(
-      'CREATE TABLE IF NOT EXISTS maintenance_magazines (id INTEGER PRIMARY KEY, id_magazine INTEGER, create_at DEFAULT CURRENT_TIMESTAMP, status TEXT, id_user INTEGER, comments TEXT, FOREIGN KEY (id_magazine) REFERENCES magazines(id), FOREIGN KEY (id_user) REFERENCES users(id) )'
+  `)
+
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS maintenance_magazines (
+      id INTEGER PRIMARY KEY,
+      id_magazine INTEGER,
+      create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      status TEXT,
+      id_user INTEGER,
+      comments TEXT,
+      FOREIGN KEY (id_magazine) REFERENCES magazines(id),
+      FOREIGN KEY (id_user) REFERENCES users(id)
     )
-    db.run(
-      'CREATE TABLE IF NOT EXISTS maintenance_dollies (id INTEGER PRIMARY KEY, id_dollie INTEGER, create_at DEFAULT CURRENT_TIMESTAMP, status TEXT, id_user INTEGER, comments TEXT, FOREIGN KEY (id_dollie) REFERENCES dollies(id), FOREIGN KEY (id_user) REFERENCES users(id) )'
+  `)
+
+  db.exec(`
+    CREATE TABLE IF NOT EXISTS maintenance_dollies (
+      id INTEGER PRIMARY KEY,
+      id_dollie INTEGER,
+      create_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+      status TEXT,
+      id_user INTEGER,
+      comments TEXT,
+      FOREIGN KEY (id_dollie) REFERENCES dollies(id),
+      FOREIGN KEY (id_user) REFERENCES users(id)
     )
-  })
+  `)
 }
 
-export const queryDatabase = async (query, params) => {
-  return new Promise((resolve, reject) => {
-    db.all(query, params, (err, rows) => {
-      if (err) {
-        reject(err)
-      } else {
-        resolve(rows)
-      }
-    })
-  })
+export const queryDatabase = (query, params) => {
+  const stmt = db.prepare(query)
+  return stmt.all(params)
 }
 
-export const insertIntoDatabase = async (query, params) => {
-  return new Promise((resolve, reject) => {
-    db.run(query, params, function (err) {
-      if (err) {
-        reject(err)
-      } else {
-        resolve({ id: this.lastID })
-      }
-    })
-  })
+export const insertIntoDatabase = (query, params) => {
+  const stmt = db.prepare(query)
+  const result = stmt.run(params)
+  return { id: result.lastInsertRowid }
 }
