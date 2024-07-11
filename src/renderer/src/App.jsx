98ecafdc486a1da4aed8fb3e6f2ee5d9d86d9602
JohanCacerez <johancacerez10@gmail.com>
2024-07-10 22:14:42 -0600

    add pages routes and react router

--- src/renderer/src/App.jsx
+++ src/renderer/src/App.jsx
@@ -1,44 +1,15 @@
-import { useEffect, useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import MenuPage from './pages/MenuPage'
 
 function App() {
-  const [users, setUsers] = useState([])
-  const [newUserName, setNewUserName] = useState('')
-
-  useEffect(() => {
-    // Obtener usuarios al cargar el componente
-    window.api.getUsers().then(setUsers)
-  }, [])
-
-  const handleAddUser = () => {
-    if (newUserName) {
-      window.api
-        .createUser(newUserName)
-        .then(() => {
-          // Actualizar la lista de usuarios después de crear uno nuevo
-          return window.api.getUsers()
-        })
-        .then(setUsers)
-      setNewUserName('')
-    }
-  }
-
   return (
-    <div>
-      <h1>Usuarios</h1>
-      <h1>development</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <input
-        type="text"
-        value={newUserName}
-        onChange={(e) => setNewUserName(e.target.value)}
-        placeholder="Nombre del usuario"
-      />
-      <button onClick={handleAddUser}>Agregar Usuario</button>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MenuPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
