d1927b7d0895ce289b4fa8b394d7ee806ea9d64b
JohanCacerez <johancacerez10@gmail.com>
2024-07-21 17:35:43 -0600

    OK

--- src/renderer/src/pages/MagazineTrackerPage.jsx
+++ src/renderer/src/pages/MagazineTrackerPage.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import MagazineMantoTracker from './MagazineTracker/MagazineMantoTracker'
 
 function MagazineTrackerPage() {
-  const [selectedOption, setSelectedOption] = useState('maintenance')
+  const [selectedOption, setSelectedOption] = useState('review')
 
   const handleSelectChange = (e) => {
     setSelectedOption(e.target.value)
@@ -21,16 +21,18 @@ function MagazineTrackerPage() {
           onChange={handleSelectChange}
           className="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
         >
+          <option value="review">Revisar Magazines</option>
           <option value="register">Registrar Magazine</option>
           <option value="maintenance">Mantenimiento Magazine</option>
-          <option value="review">Revisar Magazines</option>
           <option value="export">Extraer Base de Datos a Excel</option>
         </select>
       </div>
 
-      {selectedOption === 'maintenance' && <MagazineMantoTracker />}
+      {selectedOption === 'review' && <div> </div>}
 
-      {selectedOption === 'review' && <div>{/* Lógica para revisar magazines */}</div>}
+      {selectedOption === 'register' && <div>{/* Lógica para revisar magazines */}</div>}
+
+      {selectedOption === 'maintenance' && <MagazineMantoTracker />}
 
       {selectedOption === 'export' && <div>{/* Lógica para extraer base de datos a Excel */}</div>}
     </div>
